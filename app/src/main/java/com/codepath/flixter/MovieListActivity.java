4e8a4f97bbbf81cd58574a801d86938d628212eb
Abinaya Rajesh <arajesh@fb.com>
2017-06-23 13:35:53 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/flixter/MovieListActivity.java
@@ -0,0 +1,168 @@
+package com.codepath.flixter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.codepath.flixter.models.Config;
+import com.codepath.flixter.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
+
+import static com.loopj.android.http.AsyncHttpClient.log;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    // Base Url for API
+    public final static String API_BASE_URL = "https://api.themoviedb.org/3";
+    // parameter name for API key
+    public final static String API_KEY_PARAM = "api_key";
+
+    // tag for logging from this activity
+    public final static String TAG = "MovieListActivity";
+    // Instance fields
+    AsyncHttpClient client;
+
+    // the list of currently playing movies
+    ArrayList<Movie> movies;
+    // the recycler view
+    @BindView(R.id.rvMovies) RecyclerView rvMovies;
+    //RecyclerView rvMovies;
+    // the adapter wired to the recycler view
+    MovieAdapter adapter;
+    // image config
+    Config config;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //initialize the client
+        client = new AsyncHttpClient();
+
+        //initialize the list of movies
+        movies = new ArrayList<>();
+
+        //initialize the adapter -- movies array cannot be reinitialized after this point
+        adapter = new MovieAdapter(movies);
+
+        //resolve the recycler view and connect a layout manager and the adapter
+        //rvMovies = (RecyclerView) findViewById(rvMovies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+        rvMovies.setAdapter(adapter);
+
+
+        // get the configuration on app creation
+        getConfiguration();
+
+
+
+    }
+
+
+    // get the list of currently playing movies from the API
+    private void getNowPlaying() {
+        // create the url
+        String url = API_BASE_URL+"/movie/now_playing";
+        // set the request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));  // Always needs API key
+        // request a GET response expecting a JSON object response
+        client.get(url,params, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // load the results into movies list
+
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    for(int i=0; i<results.length(); i++){
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                        //notify adapter that a row was added
+                        adapter.notifyItemInserted(movies.size()-1);
+
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+
+                } catch (JSONException e) {
+                    logError("Failed to pase now_playing endpoint", e, true);
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get data from Now_playing endpoint", throwable, true);
+            }
+        });
+        }
+
+
+
+    // get the configuration from the API
+    private void getConfiguration(){
+        // create the url
+        String url = API_BASE_URL+"/configuration";
+        // set the request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));  // Always needs API key
+        // request a GET response expecting a JSON object response
+        client.get(url,params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                try {
+                    config = new Config(response);
+
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s",
+                            config.getImageBaseUrl(),
+                            config.getPosterSize()));
+                    // pass config to adapter
+                    adapter.setConfig(config);
+
+                    // get the now playing movie list
+                    getNowPlaying();
+
+                } catch (JSONException e) {
+                    logError("Failed parsing configuration", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                logError("Failed getting configuration", throwable, true);
+            }
+        });
+    }
+
+    // handle errors, log and alert users
+    private void logError(String message, Throwable error, boolean alertUser){
+        // always log the error
+        log.e(TAG, message, error);
+        // alert the user to avoid silent errors
+        if (alertUser){
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/codepath/flixter/models/Movie.java
@@ -0,0 +1,50 @@
+package com.codepath.flixter.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+/**
+ * Created by arajesh on 6/22/17.
+ */
+@Parcel
+public class Movie {
+
+    // values from API
+    String title;
+    String overview;
+    String posterPath; // only the path
+    String backdropPath;
+    double voteAverage;
+    Integer id;
+
+    // initialize from JSON data
+    public Movie(JSONObject object) throws JSONException {
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+        backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
+        id = object.getInt("id");
+    }
+
+    public Movie() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getBackdropPath() { return backdropPath; }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+}
